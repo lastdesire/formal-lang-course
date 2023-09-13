d6045da81d637f038bf2bed843a964b6d367de3f
lastdesire <deadinsidezz123450@mail.ru>
2023-09-13 03:20:43 +0300

    Cosmetics

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -1,11 +1,11 @@
 import os
-from project.graph_utils import *
+import project.graph_utils as utils
 import cfpq_data as cd
 from filecmp import cmp
 
 
 def test_get_graph_info() -> None:
-    graph_info = get_graph_info("travel")
+    graph_info = utils.get_graph_info("travel")
     assert 277 == graph_info.number_of_edges
     assert 131 == graph_info.number_of_nodes
     assert {
@@ -35,7 +35,7 @@ def test_get_graph_info() -> None:
 
 
 def test_get_labeled_two_cycles_graph() -> None:
-    graph = get_labeled_two_cycles_graph(5, 5, labels=("x", "y"))
+    graph = utils.get_labeled_two_cycles_graph(5, 5, labels=("x", "y"))
     assert 12 == graph.number_of_edges()
     assert 11 == graph.number_of_nodes()
     assert {"x", "y"} == set(cd.get_sorted_labels(graph))
@@ -46,7 +46,7 @@ def test_save_graph_dot() -> None:
     expected_graph_path = os.curdir + "/output/task_1/some_graph.dot"
     graph_path = os.curdir + "/output/task_1/some_other_graph.dot"
     graph = cd.labeled_two_cycles_graph(5, 5, labels=("x", "y"))
-    save_graph_dot(graph, graph_path)
+    utils.save_graph_dot(graph, graph_path)
     try:
         assert cmp(expected_graph_path, graph_path, shallow=False)
     finally:
