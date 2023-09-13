b7faa7cad55dfd24704645dad8fa6ffed37965a7
lastdesire <deadinsidezz123450@mail.ru>
2023-09-13 03:13:45 +0300

    Uploaded tests for solution of Task1

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -1,2 +1,53 @@
-def test_equal():
-    assert 1 == 1
+import os
+from project.graph_utils import *
+import cfpq_data as cd
+from filecmp import cmp
+
+
+def test_get_graph_info() -> None:
+    graph_info = get_graph_info("travel")
+    assert 277 == graph_info.number_of_edges
+    assert 131 == graph_info.number_of_nodes
+    assert {
+        "range",
+        "domain",
+        "complementOf",
+        "equivalentClass",
+        "inverseOf",
+        "comment",
+        "minCardinality",
+        "type",
+        "versionInfo",
+        "rest",
+        "oneOf",
+        "hasPart",
+        "someValuesFrom",
+        "hasAccommodation",
+        "differentFrom",
+        "subClassOf",
+        "disjointWith",
+        "first",
+        "unionOf",
+        "hasValue",
+        "intersectionOf",
+        "onProperty",
+    } == graph_info.labels
+
+
+def test_get_labeled_two_cycles_graph() -> None:
+    graph = cd.labeled_two_cycles_graph(5, 5, labels=("x", "y"))
+    assert 12 == graph.number_of_edges()
+    assert 11 == graph.number_of_nodes()
+    assert {"x", "y"} == set(cd.get_sorted_labels(graph))
+
+
+def test_save_graph_dot() -> None:
+    # There is the path to expected graph from upper test.
+    expected_graph_path = os.curdir + "/output/task_1/some_graph.dot"
+    graph_path = os.curdir + "/output/task_1/some_other_graph.dot"
+    graph = get_labeled_two_cycles_graph(5, 5, labels=("x", "y"))
+    save_graph_dot(graph, graph_path)
+    try:
+        assert cmp(expected_graph_path, graph_path, shallow=False)
+    finally:
+        os.remove(graph_path)
